20693e98a4fd2a2d8dac266c939de95483ee972e
krishnanair165 <krishnanair165@gmail.com>
2022-05-15 14:17:33 +0100

    new deatils added

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -3,9 +3,17 @@ public class MultiplicationTable {
 public static void addition(int a,int b){
     System.out.println(a+b);
 }
+
+public static void Multiplication( int a,int b){
+
+    System.out.println(a*b);
+}
     public static void main(String[] args) {
 
        addition(12,34);
+       Multiplication(13,23);
+       addition(23,45);
+       Multiplication(34,56);
 
     }
-}g
+}
