ab786214718f4a3bba96f57af0ae210c9443ac88
krishnanair165 <krishnanair165@gmail.com>
2022-05-15 13:48:04 +0100

    first commit

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable {
+
+public static void addition(int a,int b){
+    System.out.println(a+b);
+}
+    public static void main(String[] args) {
+
+       addition(12,34);
+
+    }
+}g
