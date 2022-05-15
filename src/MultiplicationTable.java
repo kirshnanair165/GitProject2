0bc3884e8fa16f5b11587ad661336141475d42e2
krishnanair165 <krishnanair165@gmail.com>
2022-05-15 14:03:28 +0100

    changes added

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,11 +1,12 @@
 public class MultiplicationTable {
 
-public static void addition(int a,int b){
-    System.out.println(a+b);
-}
+    public static void addition(int a, int b) {
+        System.out.println(a + b);
+    }
+
     public static void main(String[] args) {
 
-       addition(12,34);
+        addition(12, 34);
 
     }
-}g
+}
